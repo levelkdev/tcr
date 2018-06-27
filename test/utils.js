9e74429195f4796ab0becd0bd56e84d7dc2d9960
Mike Calvanese <mfcalvanese@gmail.com>
2018-06-27 13:26:11 -0400

    change the createChallenge function back to challenge
    better to keep it as it was in the original registry interface

--- test/utils.js
+++ test/utils.js
@@ -111,7 +111,7 @@ const utils = {
 
   challengeAndGetPollID: async (domain, actor) => {
     const registry = await Registry.deployed();
-    const receipt = await utils.as(actor, registry.createChallenge, domain, '');
+    const receipt = await utils.as(actor, registry.challenge, domain, '');
     return receipt.logs[0].args.challengeID;
   },
 
